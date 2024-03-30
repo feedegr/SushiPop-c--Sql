a77c9f1bc43acacc8c179c17566066e7380d5feb
fmelo307 <fernando.melo@outlook.com.ar>
2024-03-30 16:08:07 -0300

    Init

---
+++ 20241CYA12B-G3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20241CYA12B_G3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
