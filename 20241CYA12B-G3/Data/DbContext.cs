18432a98895e5124f1dd4741fa518e0cddfa7cda
Ornella Pacino <ornellajazminpacino@gmail.com>
2024-04-18 21:18:02 -0300

    Create DbContext

---
+++ 20241CYA12B-G3/Data/DbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _20241CYA12B_G3.Models;
+
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext (DbContextOptions<DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<_20241CYA12B_G3.Models.Carrito> Carrito { get; set; } = default!;
+
+        public DbSet<_20241CYA12B_G3.Models.CarritoItem>? CarritoItem { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Categoria>? Categoria { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Cliente>? Cliente { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Contacto>? Contacto { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Descuento>? Descuento { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Empleado>? Empleado { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Pedido>? Pedido { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Producto>? Producto { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Reclamo>? Reclamo { get; set; }
+
+        public DbSet<_20241CYA12B_G3.Models.Reserva>? Reserva { get; set; }
+
+
+    }
