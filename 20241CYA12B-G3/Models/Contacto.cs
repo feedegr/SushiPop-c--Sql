cbf50a1505251bcb1161f1b1c10174b143020165
Iipala <ivanetzja@gmail.com>
2024-04-18 23:00:17 -0300

    Adjusted new DataAnnotations in model Contacto.cs & Reclamo.cs. Also added comments with another variations.

--- 20241CYA12B-G3/Models/Contacto.cs
+++ 20241CYA12B-G3/Models/Contacto.cs
@@ -1,17 +1,31 @@
-﻿namespace _20241CYA12B_G3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _20241CYA12B_G3.Models
 {
     public class Contacto
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(255, ErrorMessage = "El maximo de longitud debe ser 255")]
         public string NombreCompleto { get; set; }
 
-        public string Email { get; set;}
+        [Required]
+        // [DataType(DataType.Text, ErrorMessage = "Unicamente Texto")]
+        // Debemos añadir una validacion para el tipo de Dato?
+        public string Email { get; set; }
 
-        public string Telefono { get; set;}
+        // [DataType(DataType.Text, ErrorMessage = "Unicamente Texto")]
+        [StringLength(10, MinimumLength = 10)]
+        public string Telefono { get; set; }
 
-        public string Mensaje { get; set;}
+        [Required]
+        [MaxLength(int.MaxValue)]
+        public string Mensaje { get; set; }
 
-        public bool Leido { get; set;} 
+        [Required]
+        // [DefaultValue(false)] Settear valor por default utilizando DataAnnotations.
+        // Otra variante es: public bool Leido { get; set;} = true / false.
+        public bool Leido { get; set; }
     }
 }
--- 20241CYA12B-G3/Models/Reclamo.cs
+++ 20241CYA12B-G3/Models/Reclamo.cs
@@ -1,13 +1,30 @@
-﻿namespace _20241CYA12B_G3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _20241CYA12B_G3.Models
 {
     public class Reclamo
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255, ErrorMessage = "Debe tener maximo 255 caracteres")]
         public string NombreCompleto { get; set; }
+
+        [Required]
+        // Debemos añadir una validacion para el tipo de Dato?
         public string Email { get; set; }
-        public string Telefono {  get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        public string Telefono { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 50)]
         public string DetalleReclamo { get; set; }
+
+        [Required]
+        // Debemos añadir una validacion para el tipo de Dato?
         public int PedidoId { get; set; }
-        public Pedido? Pedido { get; set;}
+        public Pedido? Pedido { get; set; }
     }
 }
