a40bef9412864f074d5f9ed249dd66e9bd70bbbc
Valentin <vllevinton@gmail.com>
2024-06-01 19:17:41 -0300

    Cambios en PedidosController

---
+++ 20241CYA12B-G3/Migrations/20240531232540_InitialCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _20241CYA12B_G3.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
